231f9bdb78d2d9687c9bd255d6d86c0682ee30e3
hehehbtm <1104324732@qq.com>
2017-04-08 10:37:03 +0800

    拍卖

--- src/PaiMai.java
+++ src/PaiMai.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 /**
  * Created by admin on 2017/4/8.
  */
+// 题目链接
+// http://exercise.acmcoder.com/online/online_judge_ques?ques_id=4398&konwledgeId=41
 //  题目分析
 //    暴力解法：（1）遍历价格数组，找到比当前价格高的个数，价格*个数，得出结果。
 //         时间复杂度为o(n*n)
 //    第二种解法 （1）价格排序  由数组的下标可知 比当前价格高的个数
 //            时间复杂度为 o(n*log(n))
-    
 public class PaiMai {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -27,6 +28,7 @@ public class PaiMai {
             int sum = 0;
             int res = 0;
             for (int i = 0; i < prices.length; i++) {
+//                购买人数可能大于产品数
                 int tem = prices[i]*Math.min(m,n-i);
                 if (tem > sum){
                     sum = tem ;
